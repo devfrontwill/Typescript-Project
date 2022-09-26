1f984af96fa685ecf8d7ebdbc6ebf5335d2a4d35
Wiliam Roza <devfrontwill@gmail.com>
2022-09-25 22:45:11 -0300

    feito alguns testes para afirmar a limitação do JS

---
+++ dist/js/models/negociacao.js
@@ -0,0 +1,29 @@
+export class Negociacao {
+    #data;
+    #quantidade;
+    #valor;
+
+
+    constructor(data, quantidade, valor) {
+        this.#data = data;
+        this.#quantidade = quantidade;
+        this.#valor = valor;
+    }
+
+    get data() {
+        return this.#data;
+    }
+
+    get quantidade() {
+        return this.#quantidade;
+    }
+
+    get valor() {
+        return this.#valor;
+    }
+
+    get volume() {
+        return this.#quantidade * this.#valor;
+    }
+    
+}
