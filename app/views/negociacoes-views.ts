74ee01a2e39e676b7b492414605f650306274d99
Wiliam Roza <devfrontwill@gmail.com>
2022-11-02 21:59:41 -0300

    Renderizando a View

---
+++ app/views/negociacoes-views.ts
@@ -0,0 +1,14 @@
+export class NegociacoesView {
+    template (): string {
+        return `
+        <table class="table table-hover table-bordered">
+            <thead>
+                <tr>
+                    <th>DATA</th>
+                    <th>QUANTIDADE</th>
+                    <th>VALOR</th>
+        </table>
+        
+        `;
+    }
+}
---
+++ dist/js/views/negociacoes-views.js
@@ -0,0 +1,14 @@
+export class NegociacoesView {
+    template() {
+        return `
+        <table class="table table-hover table-bordered">
+            <thead>
+                <tr>
+                    <th>DATA</th>
+                    <th>QUANTIDADE</th>
+                    <th>VALOR</th>
+        </table>
+        
+        `;
+    }
+}
